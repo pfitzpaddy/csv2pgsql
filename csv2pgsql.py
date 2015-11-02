4fa240ebefeb0d15492a8000309fbca74e1c21db
Patrick Fitzgerald <pfitzpaddy@users.noreply.github.com>
2015-11-02 14:26:43 +0430

    Update csv2pgsql.py

--- csv2pgsql.py
+++ csv2pgsql.py
@@ -8,9 +8,10 @@ if __name__ == "__main__":
 
 	# Define cmd line args
 	parser=argparse.ArgumentParser(description='''Import a CSV to Postgresql using Pandas''')
-	parser.add_argument('--connection', '-c', type=str, help='database connection string, i.e. postgresql://user:pass@localhost/db_name', required=True)
 	parser.add_argument('--file', '-f', type=str, help='path to csv file, i.e. /Users/myusername/Documents/input.csv', required=True)
-	parser.add_argument('--table', '-t', type=str, help='name of table to create in database, i.e. accounts', required=True)
+	parser.add_argument('--connection', '-c', type=str, help='database connection string, i.e. postgresql://user:pass@localhost/db_name', required=True)	
+	parser.add_argument('--schema', '-s', type=str, help='name of schema, i.e. accounts', required=True)
+	parser.add_argument('--table', '-t', type=str, help='name of table to create in database, i.e. payments', required=True)
 	args=parser.parse_args()
 
 	# connection
@@ -20,7 +21,7 @@ if __name__ == "__main__":
 	# column headers to lowercase
 	df.columns = map(str.lower, df.columns)
 	# data to sql
-	df.to_sql(args.table, engine, if_exists='replace')
+	df.to_sql(args.table, engine, schema=args.schema, if_exists='replace')
 
 	if df.size:
 		print args.table + ' import success!'
